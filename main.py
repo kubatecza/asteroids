c62d885063c3d4fa57de71542f0b0cb2a70a1caa
kubatecza <kubatecza90@gmail.com>
2025-06-22 21:12:43 +0200

    add asteroid field class; fix asteroid draw method; add asteroid field object into game loop

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
   pygame.init()
@@ -12,9 +13,11 @@ def main():
   drawable = pygame.sprite.Group()
   asteroids = pygame.sprite.Group()
   
-  Player.containers = (updatable, drawable)
-  Asteroid.containers =(updatable, drawable, asteroids)
+  Asteroid.containers = (updatable, drawable, asteroids)
+  AsteroidField.containers = (updatable)
+  asteroid_field = AsteroidField()
   
+  Player.containers = (updatable, drawable)
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
   
   dt = 0
