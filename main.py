eab6ca164cd198e617a39e4b79f0c14b51a9aaf7
kubatecza <kubatecza90@gmail.com>
2025-06-22 20:53:20 +0200

    add asteroid class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
 
 def main():
   pygame.init()
@@ -9,8 +10,10 @@ def main():
   
   updatable = pygame.sprite.Group()
   drawable = pygame.sprite.Group()
+  asteroids = pygame.sprite.Group()
   
   Player.containers = (updatable, drawable)
+  Asteroid.containers =(updatable, drawable, asteroids)
   
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
   
