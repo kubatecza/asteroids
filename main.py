b44e3e289d09da924e158c9e24408aa5af761393
kubatecza <kubatecza90@gmail.com>
2025-06-22 18:35:58 +0200

    add groups for updating and drawing

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   clock = pygame.time.Clock()
+  
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+  
+  Player.containers = (updatable, drawable)
+  
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+  
   dt = 0
   
   
@@ -15,9 +22,13 @@ def main():
       if event.type == pygame.QUIT:
         return
       
+    updatable.update(dt)
+    
     screen.fill("black")
-    player.update(dt)
-    player.draw(screen)
+
+    for unit in drawable:
+      unit.draw(screen)
+      
     pygame.display.flip()
     
     dt = clock.tick(60) / 1000
